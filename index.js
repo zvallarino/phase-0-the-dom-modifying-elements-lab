e360cb42c902935ef208e42ad6bd8acbb853a392
angeloxenakis <angeloxenakis@gmail.com>
2021-05-21 13:00:19 -0500

    commit tree 362d6a80495b4521a009723040eb9d80e268e0f4

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("#main").remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Angelo Xenakis is the champion"
